5e1d72045aee4ec27985437a9403f1ed2be3ef44
Pavel Mukhin <pauri.lentonen@gmail.com>
2018-04-19 21:46:19 +0700

    separate file for block relates stuff

--- eval/block.go
+++ eval/block.go
@@ -1,1 +1,28 @@
 package eval
+
+import (
+	"github.com/pmukhin/gophp/object"
+	"github.com/pmukhin/gophp/ast"
+)
+
+func evalBlock(ctx object.Context, node *ast.BlockStatement) (object.Object, error) {
+	var ret object.Object = nil
+	for _, st := range node.Statements {
+		r, e := Eval(st, ctx)
+		if e != nil {
+			return object.Null, e
+		}
+		// if it's return
+		if ret, ok := r.(returnObject); ok {
+			// unpack return object first...
+			return ret.value, nil
+		}
+		// add
+		ret = r
+	}
+
+	if ret == nil {
+		return object.Null, nil
+	}
+	return ret, nil
+}
