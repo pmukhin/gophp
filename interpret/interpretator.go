ca6325808bf83473329dffd739f7f8623ac5abf1
Pavel Mukhin <pauri.lentonen@gmail.com>
2018-04-20 09:50:25 +0700

    interpretor (which run files, yeah)

--- interpret/interpretator.go
+++ interpret/interpretator.go
@@ -1,1 +1,50 @@
 package interpret
+
+import (
+	"os"
+	"io/ioutil"
+	"github.com/pmukhin/gophp/scanner"
+	"github.com/pmukhin/gophp/parser"
+	"github.com/pmukhin/gophp/eval"
+	"github.com/pmukhin/gophp/object"
+)
+
+func Main(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	return runFile(f)
+}
+
+func runFile(file *os.File) error {
+	sourceCode, err := readSourceCode(file)
+	if err != nil {
+		return err
+	}
+	// remove fucking shebang
+	if string(sourceCode[:5]) == "<?php" {
+		sourceCode = sourceCode[5:]
+	}
+	p := parser.New(scanner.New(sourceCode))
+	code, e := p.Parse()
+	// parse error
+	if e != nil {
+		return e
+	}
+
+	ctx := object.NewContext(nil, object.InternalFunctionTable)
+	_, e = eval.Eval(code, ctx)
+
+	return e
+}
+
+func readSourceCode(file *os.File) ([]rune, error) {
+	srcBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	srcStr := string(srcBytes)
+
+	return []rune(srcStr), nil
+}
--- main.go
+++ main.go
@@ -5,22 +5,40 @@ import (
 	"os"
 	"fmt"
 	"github.com/pmukhin/gophp/repl"
+	"errors"
+	"github.com/pmukhin/gophp/interpret"
 )
 
 func main() {
 	root := &cobra.Command{
 		Use:   "gophp",
-		Short: "dialect of php written in go",
+		Short: "Dialect of PHP written in go",
+		Run: func(cmd *cobra.Command, args []string) {
+			e := interpret.Main(args[0])
+			if e != nil {
+				cmd.Printf("an error occured: %v\n", e)
+			}
+		},
+		// if it's just one file ... we run it
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("requires exactly one arg")
+			}
+			return nil
+		},
 	}
+
 	root.AddCommand(&cobra.Command{
 		Use:   "repl",
-		Short: "a simple REPL",
+		Short: "A simple REPL",
 		Run: func(cmd *cobra.Command, args []string) {
 			repl.Main()
 		},
 	})
 
-	if err := root.Execute(); err != nil {
+	err := root.Execute()
+
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
