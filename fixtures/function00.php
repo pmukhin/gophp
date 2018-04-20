691a94fca340d50bc4f922867c80ada1d626a6ff
Pavel Mukhin <pauri.lentonen@gmail.com>
2018-04-20 09:50:48 +0700

    fixed fixtures

---
+++ fixtures/function00.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Fibonacci recursive implementation.
+ * @author Pavel Mukhin
+ */
+function fib(int $n): int {
+    if $n < 2 { $n } else { fib($n - 1) + fib($n - 2) }
+}
+
+println(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5), fib(6), fib(7))
---
+++ fixtures/function01.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace gophp\Function;
+
+/**
+ * Fibonacci recursive implementation.
+ * @author Pavel Mukhin
+ */
+function fizzBuzz(int $n) {
+    if $n % 3 { print("Fizz") }
+    if $n % 5 { print("Buzz") }
+    println()
+}
+
+/* println(fizzBuzz) */
+foreach ( [0,1,2,3] as $val ) { fizzBuzz($val) }
